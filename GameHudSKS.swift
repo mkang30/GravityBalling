a513cd870427d1d504b2c7541563d3738e3657ee
mkang30 <57237261+mkang30@users.noreply.github.com>
2020-03-24 01:18:47 -0400

    Comments added

--- GameHudSKS.swift
+++ GameHudSKS.swift
@@ -1,9 +1,17 @@
 import Foundation
 import SpriteKit
 import SceneKit
+/**
+* This class models HUDs used in the game
+* extends from SKScene as HUDs are 2D
+* components.
+*/
 public class GameHudSKS: SKScene {
     var scoreHud: SKLabelNode!
     var title: SKLabelNode!
+    /*
+    * Uploading the display to the main view
+    */
     override public func sceneDidLoad(){
         super.sceneDidLoad()
         if let highscore: SKLabelNode = childNode(withName: "hud") as! SKLabelNode? {
@@ -14,9 +22,15 @@ public class GameHudSKS: SKScene {
         }
         
     }
+    /*
+    * Updates the hud (called when the score or highscore change)
+    */
     public func updateHud(balls: Int) {
         scoreHud.text = "Highscore: \(Data.highScore) Score: \(Data.score) ⚽️: \(balls)"
     }
+    /*
+    * Self-explanatory
+    */
     public func setTitle(state: Int){
         if state == 1 {
             title.text = "Earth"
